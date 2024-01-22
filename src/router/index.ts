f04fec13cb7ce7e54050cacac4dc97342fe888ca
Stefano Sanna <stefano.sanna@spindox.it>
2024-01-22 18:00:01 +0100

    modificato route per autenticazione

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/about',
@@ -15,9 +16,30 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = true;
+
+  if (to.name === "login" && isAuthenticated){
+    next('/')
+  }
+
+  if (requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else {
+    next();
+  }
 })
 
 export default router
